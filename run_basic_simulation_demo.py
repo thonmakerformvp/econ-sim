d5840b49b80e00f2b81efa35a14a631d3914c475
thonmakerformvp <mark@marks-air.dynamic.ucsd.edu>
2017-11-05 01:13:29 -0700

    fixed bugs to make it seemingly work on a basic test

--- run_basic_simulation_demo.py
+++ run_basic_simulation_demo.py
@@ -1,21 +1,21 @@
 import csv
 
 def demand_function(quantity, time):
-	return 100 - quantity + int((time%5))
+	return 100 - quantity + int((time/5))
 
 def supply_function(time):
 	#perfectly competitive market so set equal to margian cost. marginal cost will drop with time though thanks to technology advancements hence the -)
-	return 50.00 - (time*0.0000001)
+	return 50.00 - int((time/100))
 
 def inverse_demand_function(price, time):
-	return 100 + int((time%5)) - price
+	return 100 + int((time/5)) - price
 
 def inverse_supply_function(time):
-	return 50.00 + (time*0.0000001) + int((time%5))
+	return 50.00 + int((time/100)) + int((time/5))
 
 def equilibrium_quantity(time):
 	#pass the value from this into the demand_function or supply_function to get equilirbium price
-	return 50 + int((time%5)) + (time*0.0000001) 
+	return 50 + int((time/5)) + int((time/100)) 
 
 ofilePosts = open('simulation_results.csv', 'w')
 writerPosts = csv.writer(ofilePosts, delimiter=',')
@@ -25,7 +25,7 @@ writerPosts.writerow(['t', 'p', 'q', 'in equilibrium', 'p_e', 'q_e'])
 initial_p = 32.05
 max_price_update = 8.00
 
-duration_of_simulation = 1000
+duration_of_simulation = 10
 
 is_first = True
 for t in range(0, duration_of_simulation):
@@ -45,7 +45,7 @@ for t in range(0, duration_of_simulation):
 		elif p > p_e:
 			q = inverse_demand_function(p, t)
 	else:
-		if p_e != p:
+		if p_e != p or q_e != q:
 			if abs(p_e - p) < max_price_update:
 				in_equilibrium = True
 				p = p_e
@@ -65,6 +65,13 @@ for t in range(0, duration_of_simulation):
 			in_equilibrium = False
 			q_e = equilibrium_quantity(t)
 			p_e = demand_function(q_e, t)
+	print("time: " + str(t))
+	print("price:" + str(p))
+	print("quantity:" + str(q))
+	print("is in equilibrium: " + str(in_equilibrium))
+	print('equilibrium price:' + str(p_e))
+	print('equilibrium quantity: ' + str(q_e))
+	print('--------------')
 	writerPosts.writerow([t, p, q, in_equilibrium, p_e, q_e])
 
 
